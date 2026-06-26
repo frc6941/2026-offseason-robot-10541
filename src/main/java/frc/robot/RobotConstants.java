3e021aad74c947c82c6b8dc2e5d54d9a3df88e2b
AlltAWD <hanson_2014@hotmail.com>
2026-06-26 19:31:56 +0800

    FIxed Canivore name

--- src/main/java/frc/robot/RobotConstants.java
+++ src/main/java/frc/robot/RobotConstants.java
@@ -5,7 +5,7 @@ import com.ctre.phoenix6.CANBus;
 public final class RobotConstants {
     // CAN
     public static final String ROBORIO_CAN_BUS_NAME = "rio";
-    public static final String CANIVORE_CAN_BUS_NAME = "6941Canivore0";
+    public static final String CANIVORE_CAN_BUS_NAME = "10541Canivore0";
     public static final CANBus CANIVORE_CAN_BUS = new CANBus(CANIVORE_CAN_BUS_NAME);
 
 
