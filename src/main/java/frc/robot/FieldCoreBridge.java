43909d488e8dc2988f5d4ad2ea86a8a5b78017be
LouiscaiKM7 <yooyooyoojun@163.com>
2026-07-13 22:18:07 +0800

    spotless

--- src/main/java/frc/robot/FieldCoreBridge.java
+++ src/main/java/frc/robot/FieldCoreBridge.java
@@ -10,7 +10,6 @@ import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.subsystems.Hopper.HopperConfig;
 import frc.robot.subsystems.Hopper.HopperParamsNT;
 import frc.robot.subsystems.Hopper.HopperSubsystem;
 import frc.robot.subsystems.Intaker.IntakerConfig.IntakeMode;
